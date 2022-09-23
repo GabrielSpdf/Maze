a5be0ec97637aa3c09bc91f8fccb1d6b8b592058
GabrielSpdf <gb.spada@hotmail.com>
2022-09-23 20:07:38 -0300

    Faltando: passagem de nivel e invisibilidade

--- Mazeee/Maze.c
+++ Mazeee/Maze.c
@@ -7,31 +7,74 @@ int main()
 	Escape saida;
 	Tree* arvore;
 
+    int escolher=0;
     int modo;
     int **mapa; 
 
     printf("Selecione a dificuldade: \n");
     printf("1 - Facil\n2 - Medio\n3 - Dificil\n");
-    int escolher;
-
-    scanf("%d", &escolher);
-
-    if(escolher == 1)
+    
+    while(escolher != 1 && escolher != 2 && escolher != 3)
     {
-         printf("oi");
-        modo = FACIL;
-        mapa = (int**) malloc (FACIL * sizeof(int*));
-        for(int i=0; i<FACIL; i++)
+        scanf("%d", &escolher);
+        
+        if(escolher == 1)
         {
-            mapa[i] = (int*) malloc (FACIL * sizeof(int));
+            modo = INTRODUCAO;
+            mapa = (int**) malloc (INTRODUCAO * sizeof(int*));
+            for(int i=0; i<INTRODUCAO; i++)
+            {
+                mapa[i] = (int*) malloc (INTRODUCAO * sizeof(int));
+            }
+
+            for(int i=0; i<INTRODUCAO; i++)
+            {
+                for(int j=0; j<INTRODUCAO; j++)
+                {
+                    mapa[i][j] = mapa1[i][j];
+                }
+            }
         }
 
-        for(int i=0; i<FACIL; i++)
+        else if(escolher == 2)
         {
-            for(int j=0; j<FACIL; j++)
+            modo = MEDIO;
+            mapa = (int**) malloc (MEDIO * sizeof(int*));
+            for(int i=0; i<MEDIO; i++)
             {
-                mapa[i][j] = mapa1[i][j];
+                mapa[i] = (int*) malloc (MEDIO * sizeof(int));
             }
+
+            for(int i=0; i<MEDIO; i++)
+            {
+                for(int j=0; j<MEDIO; j++)
+                {
+                    mapa[i][j] = mapa2[i][j];
+                }
+            }  
+        }
+
+        else if(escolher == 3)
+        {
+            modo = DIFICIL;
+            mapa = (int**) malloc (DIFICIL * sizeof(int*));
+            for(int i=0; i<DIFICIL; i++)
+            {
+                mapa[i] = (int*) malloc (DIFICIL * sizeof(int));
+            }
+
+            for(int i=0; i<DIFICIL; i++)
+            {
+                for(int j=0; j<DIFICIL; j++)
+                {
+                    mapa[i][j] = mapa3[i][j];
+                }
+            }
+        }
+
+        else
+        {
+            printf("Escolha uma dificuldade valida!\n");
         }
     }
     
@@ -56,11 +99,11 @@ int main()
 		s = baixo
 		d = direita*/
 
-		if(input == 'd') /*Mover o jogador para direita*/
+		if(input == 'w') /*Mover o jogador para cima*/
 		{
-			if(mapa[jogador.x][jogador.y+1] != 0 && mapa[jogador.x][jogador.y+1] != -3)
+			if(mapa[jogador.x-1][jogador.y] != 0 && mapa[jogador.x-1][jogador.y] != -3)
 			{
-				jogador.y++;
+				jogador.x--;
 				arvore = insert_element(arvore, mapa[jogador.x][jogador.y]); /*Insere o valor na arvore, valor o qual o jogador acabou de passar por "cima"*/
 			}
 		}
@@ -74,20 +117,21 @@ int main()
 			}
 		}
 
-		else if(input == 'w') /*Mover o jogador para cima*/
-		{
-			if(mapa[jogador.x-1][jogador.y] != 0 && mapa[jogador.x-1][jogador.y] != -3)
-			{
-				jogador.x--;
-				arvore = insert_element(arvore, mapa[jogador.x][jogador.y]); /*Insere o valor na arvore, valor o qual o jogador acabou de passar por "cima"*/
-			}
-		}
 
 		else if(input == 's') /*Mover o jogador para direita*/
 		{
 			if(mapa[jogador.x+1][jogador.y] !=0 && mapa[jogador.x+1][jogador.y] != -3)
 			{
 				jogador.x++;
+				arvore = insert_element(arvore, mapa[jogador.x][jogador.y]); /*Insere o valor na arvore, valor o qual o jogador acabou de passar por "cima"*/
+			}
+		}
+
+        else if(input == 'd') /*Mover o jogador para direita*/
+		{
+			if(mapa[jogador.x][jogador.y+1] != 0 && mapa[jogador.x][jogador.y+1] != -3)
+			{
+				jogador.y++;
 				arvore = insert_element(arvore, mapa[jogador.x][jogador.y]); /*Insere o valor na arvore, valor o qual o jogador acabou de passar por "cima"*/
 			}
 		}
